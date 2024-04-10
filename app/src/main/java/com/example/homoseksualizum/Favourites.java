4d962921428fedcc1e88f6261171f2a6de9b7e3b
Goshopikata <guyfiery53@gmail.com>
2024-04-11 00:05:56 +0300

    lija na barov vsekidnevno

--- app/src/main/java/com/example/homoseksualizum/Favourites.java
+++ app/src/main/java/com/example/homoseksualizum/Favourites.java
@@ -1,13 +1,22 @@
 package com.example.homoseksualizum;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Favourites extends AppCompatActivity {
+    RecyclerView recyclerView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favourites);
-
+    recyclerView = findViewById(R.id.patka2);
+    Button goBack = findViewById(R.id.return123);
+    Intent back = new Intent(this, MainActivity.class);
+        goBack.setOnClickListener(v -> {startActivity(back);});
 }
+
 }
